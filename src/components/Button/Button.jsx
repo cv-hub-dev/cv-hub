4246729fc3fce94a6b4decde8f266884c9fca154
Anastasia Kashkinova <anastasia.kashkinova@gmail.com>
2023-02-05 21:49:20 +1100

    add button variants

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,8 +2,8 @@ import * as React from "react"
 import "./Button.scss"
 import { Link } from "gatsby"
 
-const Button = ({to = "/", children}) => (
-  <Link to={to} className='button'>
+const Button = ({to = "/", children, variant = `primary`}) => (
+  <Link to={to} className={`button ${variant === `primary` ? `buttonPrimary` : `buttonSecondary`}`}>
     {children}
   </Link>
 )
