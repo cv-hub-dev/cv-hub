c7e645c7e7fbd929b2eba3279f1e91ed46180446
bates33 <hi@bates33.dev>
2023-02-19 14:23:14 +0800

    update education page

--- src/pages/education/index.js
+++ src/pages/education/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Formik, Form } from "formik"
+import { Formik, Form, FieldArray } from "formik"
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import TextArea from "../../components/TextArea";
@@ -11,32 +11,62 @@ const EducationPage = () => {
   const { storedValue: educationValues, setValue: setEducationValues } = useLocalStorage("education")
 
   const initialValues = {
-    schoolName: "",
-    degree: "",
-    startDate: "",
-    endDate: "",
-    description: "",
-  }
+    education: [
+      {
+        schoolName: "",
+        degree: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+      },
+    ],
+  };
 
   return (
     <div className="duo-layout">
-      <Formik initialValues={educationValues || initialValues}>
-        {({ values }) => (
+      <Formik
+        initialValues={educationValues || initialValues}
+        onSubmit={(values) => {
+          setEducationValues(values.education);
+        }}
+      >
+        {({ values, errors, touched }) => (
           <Form className="form" onChange={(changedValue) => handleOnChange({ values, changedValue, setValues: setEducationValues })}>
             <h3>Education</h3>
-            <Input name="schoolName" required type="text" label="School Name" />
-            <Input name="degree" required type="text" label="Degree" />
-            <Input name="startDate" required type="date" label="Start Date" />
-            <Input name="endDate" required type="date" label="End Date" />
-            <TextArea name="description" required type="text" label="Description" />
+
+            <FieldArray name="education">
+              {({ push, remove }) => (
+                <div>
+                  {values.education.map((_, index) => (
+                    <div key={index}>
+                      <Input name={`education[${index}].schoolName`} required type="text" label="School Name" />
+                      <Input name={`education[${index}].degree`} required type="text" label="Degree" />
+                      <Input name={`education[${index}].startDate`} required type="date" label="Start Date" />
+                      <Input name={`education[${index}].endDate`} required type="date" label="End Date" />
+                      <TextArea name={`education[${index}].description`} required type="text" label="Description" />
+                      {values.education.length > 1 && (
+                        <Button variant="secondary" onClick={() => remove(index)}>
+                          Remove
+                        </Button>
+                      )}
+                    </div>
+                  ))}
+                  {values.education.length < 3 && (
+                    <Button variant="secondary" onClick={() => push(initialValues.education[0])}>
+                      Add Education
+                    </Button>
+                  )}
+                  {errors.education && touched.education && <div>{errors.education}</div>}
+                </div>
+              )}
+            </FieldArray>
 
             <div className="buttonsWrapper">
               <Button variant="secondary" to="/personal">Back</Button>
-              <Button to="/">Next</Button>
+            <Button to="/">Next</Button>
             </div>
           </Form>
         )}
-        
       </Formik>
       <Preview />
     </div>
